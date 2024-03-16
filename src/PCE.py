minor bugfixes, fixed type on CV calculation [minor]

src/PCE.py
@@ -86,10 +86,10 @@ class PCE():
         vals = self.PCE(samples[:, 0], samples[:, 1])
         
         mean_pce = np.mean(vals)
-        var_pce = np.std(vals, ddof=1)
+        sigma_pce = np.std(vals, ddof=1)
 
         if np.abs(mean_pce) < 1e-9:
-            return np.sqrt(var_pce)
+            return sigma_pce
         else:
             # should we have abs here?
-            return np.sqrt(var_pce)/np.abs(mean_pce)
+            return sigma_pce/np.abs(mean_pce)

src/optimize.py
@@ -24,9 +24,9 @@ class Optimize():
     def optimization_step(self):
         z_next, f_z_next = self.KL_GP.z_next(n_starting_points=self.n_starting_points_z)
         print(z_next, f_z_next) 
-        if np.abs(f_z_next) < 1e-6:
-            print('Negligible improvement', z_next, f_z_next)
-            return
+        #if np.abs(f_z_next) < 1e-6:
+        #    print('Negligible improvement', z_next, f_z_next)
+        #    return
         self.KL_GP.add_z(z_next)
 
         z_DoE, Pmin = self.KL_GP.Pmin()

test_sellar.py
@@ -30,10 +30,10 @@ DoE_z = LHS(xlimits=problem.lims_z, random_state=20)(20)
 klgp = KL_GP(DoE_z, sproblem, 3)
 
 optim = Optimize(klgp, n_starting_points_z=20)
+#optim.KL_GP.add_z(np.array([0., 2.634, 0.]))
 
-for i in tqdm(range(10)):
+for i in tqdm(range(20)):
     #fig, ax = plotter.plot_surrogates(optim.KL_GP, -3., 'img/%d_surrogate.png' % i)
-    optim.KL_GP.add_z(np.array([0., 2.634, 0.]))
     print(optim.optimization_step())
     print(optim.KL_GP.SCP.y1.DoE_z)
 